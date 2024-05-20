61fceca43e808cb8e8444d0b8a6e9e97e245e8d8
yohanmira1 <ymira@poligran.edu.co>
2024-05-20 11:24:09 -0500

    firts commit

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+const port = 3000;
+
+// motor de plantillas
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+
+app.use(express.static(__dirname + "/public"));
+
+
+
+app.get('/', (req, res)=>{
+    res.render("index", {titulo: "mi titulo dinámico"})
+});
+
+app.get('/servicios', (req, res)=>{
+    res.render("servicios", {tituloServicio: "Este es un mensaje dinámico de servicio"})
+});
+
+app.use((req, res, next)=>{
+    res.status(404).render("404", {
+        titulo: "404",
+        descripcion: "Título del sitio web"
+    });
+})
+
+app.listen(port, ()=> {
+    console.log ('servidor a su servicio en el puerto', port)
+});
