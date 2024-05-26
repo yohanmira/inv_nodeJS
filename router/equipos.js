019f7c4947a66e4492fe5b467166cad1e01b1259
yohanmira <yamira3@misena.edu.co>
2024-05-26 10:24:46 -0500

    Crud se creo boton de editar

--- router/equipos.js
+++ router/equipos.js
@@ -43,4 +43,24 @@ router.post('/', async(req,res)=>{
     }
 })
 
+router.get('/:id', async(req, res)=>{
+    const id = req.params.id;
+    try {
+
+        const equiposDB = await equipos.findOne({_id: id});
+        console.log(equiposDB);
+
+        res.render('detalle', {
+            equipo: equiposDB,
+            error: false
+        })
+        
+    } catch (error) {
+        console.log(error)
+        res.render('detalle', {
+            error: true,
+            mensaje: 'No se encuentra el id seleccionado'
+        })
+    }
+})
 module.exports = router;
